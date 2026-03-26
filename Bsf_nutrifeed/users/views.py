3f5ab3fd90fb32eeb3c6ffde814d39abc6547da7
Sjayp <Sjaypeter.sjp@gmail.com>
2026-03-26 12:56:32 +0100

    feat:add views for user app

--- Bsf_nutrifeed/users/views.py
+++ Bsf_nutrifeed/users/views.py
@@ -1,3 +1,79 @@
 from django.shortcuts import render
+from rest_framework import generics, status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.tokens import RefreshToken
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
+from .models import User
+from .serializers import RegisterSerializer, UserProfileSerializer, ChangePasswordSerializer
 
-# Create your views here.
+
+
+class RegisterView(generics.CreateAPIView):
+    """Register a new farmer or admin account"""
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+    permission_classes = [permissions.AllowAny]
+
+    def create(self, request, *args, **kwargs):
+        serializer =self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user= serializer.save()
+        refresh = RefreshToken.for_user(user)
+        return Response(
+            {
+                "message": "Registration succesful.",
+                "user": UserProfileSerializer(user).data,
+                "tokens": {
+                    "refresh": str(refresh),
+                    "access": str(refresh.access_token),
+                },
+
+            },
+            status=status.HTTP_201_CREATED,
+        )
+    
+
+class LoginView(TokenObtainPairView):
+    permission_classes = [permissions.AllowAny]
+
+
+class LogoutView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+        except Exception:
+            return Response({"mesasge": "Logged out succesfully."}, status=status.HTTP_200_OK)
+        
+
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+    
+
+class ChangePasswordView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self,request):
+        serializer = ChangePasswordSerializer(data=request.data, context={"request": request})
+        serializer.is_valid(raise_exception=True)
+        request.user.set_password(serializer.validated_data["new password"])
+        request.user.save()
+        return Response({"message": "Password updated succesfully"})
+
+
+class UserListView(generics.ListAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [permissions.IsAdminUser]
+    queryset = User.objects.all()
+    
+
