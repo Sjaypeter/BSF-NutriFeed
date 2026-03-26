91b4aad67c5a15fc033b3b67b87caadf52fc34f3
Sjayp <Sjaypeter.sjp@gmail.com>
2026-03-26 12:59:05 +0100

    feat:add admin control for user app

--- Bsf_nutrifeed/users/admin.py
+++ Bsf_nutrifeed/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ["username", "email", "role", "farm_name", "is_verified", "date_joined"]
+    list_filter = ["role", "is_verified", "is_staff"]
+    fieldsets = BaseUserAdmin.fieldsets + (
+        ("BSF-Nutrifeed Profile", {
+            "fields": ("role", "phone_number", "farm_name", "farm_location", "is_verified")
+        }),
+    )
