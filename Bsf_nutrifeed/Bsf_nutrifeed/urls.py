7fd4b3b995e93b3b4ea40f3db9f08d0e7863c9cf
Sjayp <Sjaypeter.sjp@gmail.com>
2026-03-26 11:54:23 +0100

    feat:add swagger to urls.py

--- Bsf_nutrifeed/Bsf_nutrifeed/urls.py
+++ Bsf_nutrifeed/Bsf_nutrifeed/urls.py
@@ -15,8 +15,40 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenRefreshView
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework.permissions import AllowAny
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="BSF-Nutrifeed API",
+        default_version="V1",
+        description=(
+            "Backend API for BSF-Nutrifeed platform"
+            "Solution for sustainable poultry feed production"
+        ),
+        contact=openapi.Contact(email="Sjaypeter.sjp@gmail.com"),
+        license=openapi.License(name="MIT")
+
+    ),
+    public=True,
+    permission_classes=(AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    #Auth
+    path("api/auth", include("users.urls")),
+    path("api/auth/token/refresh", TokenRefreshView.as_view(), name="token_refresh"),
+    #Core resources
+    path("api/feed", include("feed_production.urls")),
+    path("api/monitoring", include("monitoring.urls")),
+
+    #Swagger/ Redoc
+    path("swagger/", schema_view.with_ui("swagger", cache_timeout=0), name="swagger-ui"),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="redoc"),
+    path("swagger.json",schema_view.without_ui(cache_timeout=0), name="swagger-json"),
 ]
